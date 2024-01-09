5bbd31e9e1114f6d316ffb06c01d0ebfda3b25f7
vts1978 <vinicioussouza@gmail.com>
2024-01-09 16:44:28 -0300

    Verify if author and genre exist before begintransaction, if doesnt send notfound message, 404 statuscode

--- Application/Commands/Books/CreateBookCommandHandler.cs
+++ Application/Commands/Books/CreateBookCommandHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Commands.Genres;
+using Application.Errors;
 using Domain.Entities;
 using Domain.Interfaces;
 using Domain.Result;
@@ -22,6 +23,14 @@ namespace Application.Commands.Books
         public async Task<Result<int>> Handle(CreateBookCommand request, CancellationToken cancellationToken)
         {
             Book book = new(request.BookId, request.Titulo, request.AuthorId, request.GenreId);
+
+            var genre = await _unitOfWork.Genre.GetGenreByIdAsync(book.GenreId);
+            if (genre is null)
+                return Result<int>.Failure(BookErrors.NotFoundDonorGenre, ResultStatusCodeEnum.NotFound);
+
+            var author = await _unitOfWork.Author.GetAuthorByIdAsync(book.AuthorId);
+            if (author is null)
+                return Result<int>.Failure(BookErrors.NotFoundDonorAuthor, ResultStatusCodeEnum.NotFound);
 
             await _unitOfWork.BeginTransactionAsync();
             await _unitOfWork.Book.CreateBookAsync(book);
