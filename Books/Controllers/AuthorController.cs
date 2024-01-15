9288e7fcd238ec2c84d5e5b1e43d0986d12b4cac
vts1978 <vinicioussouza@gmail.com>
2024-01-15 18:31:33 -0300

    New endpoint for search by name(Author)

--- Books/Controllers/AuthorController.cs
+++ Books/Controllers/AuthorController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Application.Commands.Authors.CreateAuthor;
 using Application.Commands.Authors.DeleteAuthor;
 using Application.Commands.Authors.UpdateAuthor;
+using Application.Queries.Author;
+using Application.Queries.Author.GetAuthorByIdQuery;
+using Application.Queries.Author.GetAuthorsByName;
+using Domain.Helpers;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,12 +20,37 @@ namespace Books.Controllers
         {
         }
 
-        [HttpPost]
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAll([FromQuery] PageQuery pageQuery)
+        {
+            var query = new GetAllAuthorQuery(pageQuery);
+            var result = await _mediator.Send(query);
+            return StatusCode((int)result.StatusCode, result.GetFinalObject());
+        }
+
+        [HttpGet("getById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetAuthorByIdQuery(id);
+            var result = await _mediator.Send(query);
+            return StatusCode((int)result.StatusCode, result.GetFinalObject());
+        }
+
+        [HttpGet("getByName")]
+        public async Task<IActionResult> GetByName([FromQuery] PageQuery pageQuery, string name)
+        {
+            var query = new GetAuthorsByNameQuery(name, pageQuery);
+            var result = await _mediator.Send(query);
+            return StatusCode((int)result.StatusCode, result.GetFinalObject());
+        }
+
+        [HttpPost("create")]
         public async Task<IActionResult> Create(CreateAuthorCommand command)
         {
             var result = await _mediator.Send(command);
             return StatusCode((int)result.StatusCode, result.GetFinalObject());
         }
+
 
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> Delete(int id)
