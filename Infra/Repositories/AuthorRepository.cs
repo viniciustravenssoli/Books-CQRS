937daecf2dbf922c0a60509907545e4df05af329
vts1978 <vinicioussouza@gmail.com>
2024-01-15 18:29:08 -0300

    Implementing methods to authors queries

--- Infra/Repositories/AuthorRepository.cs
+++ Infra/Repositories/AuthorRepository.cs
@@ -20,14 +20,31 @@ namespace Infra.Repositories
             return author.Id;
         }
 
-        public Task<List<Author>> GetAllAsync(int top, int skip)
+        public async Task<List<Author>> GetAllAsync(int top, int skip)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Authors
+                .Skip(skip)
+                .Take(top)
+                .AsNoTracking()
+                .ToListAsync();
         }
 
-        public Task<List<Author>> GetAllByName(int top, int skip, string name)
+        public async Task<List<Author>> GetAllByName(int top, int skip, string name)
         {
-            throw new NotImplementedException();
+            var filteredAuthors = await _dbContext.Authors
+                .Where(a => EF.Functions.Like(a.Name, $"%{name}%"))
+                .Skip(skip)
+                .Take(top)
+                .ToListAsync();
+
+            // only work with lowercase
+            // var filteredAuthors = await _dbContext.Authors
+            //     .Where(a => a.Name.Contains(name))
+            //     .Skip(skip)
+            //     .Take(top)
+            //     .ToListAsync();
+            
+            return filteredAuthors;
         }
 
         public async Task<Author> GetAuthorByIdAsync(int id)
