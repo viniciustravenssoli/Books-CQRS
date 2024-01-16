6fd56bfdd8b4c53f488702f89eb64f8ce8d6fdce
vts1978 <vinicioussouza@gmail.com>
2024-01-16 16:26:58 -0300

    Adding methods get bookcontroller

--- Books/Controllers/BookController.cs
+++ Books/Controllers/BookController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Application.Commands.Books;
 using Application.Commands.Books.Delete;
+using Application.Queries.Author;
+using Application.Queries.Book.GetAllBook;
+using Application.Queries.Book.GetBookByAuthor;
+using Application.Queries.Book.GetBookById;
+using Domain.Helpers;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +24,30 @@ namespace Books.Controllers
         public async Task<IActionResult> Create(CreateBookCommand command)
         {
             var result = await _mediator.Send(command);
+            return StatusCode((int)result.StatusCode, result.GetFinalObject());
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAll([FromQuery] PageQuery pageQuery)
+        {
+            var query = new GetAllBookQuery(pageQuery);
+            var result = await _mediator.Send(query);
+            return StatusCode((int)result.StatusCode, result.GetFinalObject());
+        }
+
+        [HttpGet("getById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetBookByIdQuery(id);
+            var result = await _mediator.Send(query);
+            return StatusCode((int)result.StatusCode, result.GetFinalObject());
+        }
+
+        [HttpGet("get-all-by-AuthorId/{authorId}")]
+        public async Task<IActionResult> GetAllByAuthorId([FromQuery] PageQuery pageQuery, int authorId)
+        {
+            var query = new GetAllBookByAuthorQuery(pageQuery, authorId);
+            var result = await _mediator.Send(query);
             return StatusCode((int)result.StatusCode, result.GetFinalObject());
         }
 
